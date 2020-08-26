ba93d9098d0e88a763288214ba90dc6eddbe137e
pavankarthik1023 <64345984+pavankarthik1023@users.noreply.github.com>
2020-08-26 07:54:09 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,24 @@
+class Paper{
+    constructor(x,y,radius){
+    var  options={
+    isStatic:false,
+    restitution:0.3,
+    friction:5,
+    density:1.2,
+    
+    }
+    this.body=Bodies.circle(x,y,radius,options)
+    World.add(world,this.body);
+
+    this.body.velocityY=7;
+    }
+    display(){
+       
+       fill("red");
+       ellipseMode(RADIUS);
+       ellipse(this.body.position.x,this.body.position.y,40,40);
+       
+    }
+
+    
+    }
